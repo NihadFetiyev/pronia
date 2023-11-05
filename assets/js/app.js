f28b36253012d632721a45ca403c0f3330181126
NihadFatiyev <tu7NAAWDF@code.edu.az>
2023-11-05 22:33:11 +0400

    exhausted

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+//header dropdown
 const currency = document.querySelector(".currency")
 const lang = document.querySelector(".lang")
 const subCurrency = document.querySelector(".subCurrency")
@@ -29,3 +30,43 @@ window.addEventListener("click", () => {
         subLang.classList.remove("active")
     }
 })
+
+// navin scroll etdikde gelmesi
+let navSroll = document.querySelector(".nav-scroll")
+
+window.onscroll = function () {
+    scrollFunction()
+    backFunction()
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        navSroll.style.top = "48px"
+        navSroll.style.height = "100px"
+        navSroll.style.opacity = "1"
+    } else {
+        navSroll.style.top = "48px"
+        navSroll.style.height = "0px"
+        navSroll.style.opacity = "0"
+    }
+};
+
+
+// back scroll etdikde top sehifeye getmek ucun
+const back = document.querySelector(".back")
+
+let scrolling = window.pageYOffset;
+
+
+function backFunction() {
+    let currentScrollPos = window.pageYOffset;
+
+    if (scrolling > currentScrollPos) {
+
+        back.style.bottom = "40px";
+        back.style.opacity = "1";
+    } else {
+        back.style.bottom = "0px";
+        back.style.opacity = "0";
+    }
+    scrolling = currentScrollPos;
+}
